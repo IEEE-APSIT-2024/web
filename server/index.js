6f89cde96de56c7868b96b8dd43e881ac668692f
Ayushpani <ayushpanigrahi84@gmail.com>
2024-03-17 13:08:34 +0530

    made changes in server/index.js , i.e. backend side and added code for registering and signing in the user
    also made a new file for defining the schema

--- server/index.js
+++ server/index.js
@@ -7,3 +7,29 @@ const PORT = 5000
 mongoose.connect(CONNECTION_URL)
     .then(() => app.listen(PORT, () => console.log('Database connected')))
     .catch((error) => console.log(error.message));
+
+
+    app.use(express.json());
+
+    app.post('/signup', async (req, res) => {
+        const { username, email, password } = req.body;
+        try {
+
+            const newUser = new User({ username, email, password });
+
+            await newUser.save();
+            res.status(201).json(newUser);
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    });
+    
+    app.get('/signin', async (req, res) => {
+        try {
+
+            const users = await User.find();
+            res.json(users);
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    });
